62448b526fbaec8f3c4baa8546fc1ffa354b191a
sr-sagar <sagarramani992@gmail.com>
2025-07-20 14:24:38 +0530

    added frontend link to cors in index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["https://srcollect.netlify.app", "http://localhost:5173"],
 }));
 app.use(express.urlencoded({extended: true}));
 app.use(generalRateLimiting)
